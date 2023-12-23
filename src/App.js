ccf9dd34845bc0d07c034efbbb1f4478fcc8ceab
Ali Ramazon <mr.safarali@gmail.com>
2023-12-23 18:17:14 -0500

    Create api calls to communicate with Admin service

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useEffect } from "react";
+import { admin } from "./api";
 import { Typography, TypographyProps } from "./design-system";
 
 const App = () => {
+    useEffect(() => {
+        admin
+            .forgotPassword("ramazoncode@gmail.com")
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
     return (
         <>
-            <Typography variant={TypographyProps.variant.h2}>Hello</Typography>;
-            <Typography variant={TypographyProps.variant.subtitleLG}>
-                Hello
-            </Typography>
+            <Typography variant={TypographyProps.variant.h1}>Hello</Typography>;
         </>
     );
 };

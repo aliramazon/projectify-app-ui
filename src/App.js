478bb2e424212be8230fb65a448748a83ca590b6
Ali Ramazon <mr.safarali@gmail.com>
2023-12-23 16:54:24 -0500

    Clean project before development

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Typography, TypographyProps } from "./design-system";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <>
+            <Typography variant={TypographyProps.variant.h2}>Hello</Typography>;
+            <Typography variant={TypographyProps.variant.subtitleLG}>
+                Hello
+            </Typography>
+        </>
+    );
+};
 
 export default App;

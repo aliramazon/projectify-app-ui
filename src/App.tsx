48d88eeeb14bbb73b7bbe7c2319ee8609d937667
Ali Ramazon <mr.safarali@gmail.com>
2023-12-29 19:01:34 -0700

    Create reusable wrapper component for Auth pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,7 @@
-import { useState } from "react";
-import { Button, Typography, Input, Modal } from "./design-system";
+import { Signup } from "./pages";
 
 const App = () => {
-    const [showModal, setShowModal] = useState<boolean>(false);
-
-    return (
-        <div style={{ padding: "100px" }}>
-            <Button onClick={() => setShowModal(true)}>Open Modal</Button>
-            <Modal show={showModal} position="right">
-                <span onClick={() => setShowModal(false)}>X</span>
-                <form
-                    style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        gap: "15px",
-                    }}
-                >
-                    <Typography variant="h6">Hello</Typography>
-                    <Input
-                        placeholder="Email"
-                        type="email"
-                        value=""
-                        onChange={(char) => console.log(char)}
-                        shape="rounded"
-                        size="lg"
-                    />
-
-                    <Button
-                        color="primary"
-                        shape="rounded"
-                        onClick={() => setShowModal(false)}
-                    >
-                        Submit
-                    </Button>
-                </form>
-            </Modal>
-        </div>
-    );
+    return <Signup />;
 };
-
-<button type="submit"></button>;
 
 export { App };

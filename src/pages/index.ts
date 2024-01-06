1443af8d0f32d9b0f3757c4241a70decaed54216
Ali Ramazon <mr.safarali@gmail.com>
2024-01-05 21:42:33 -0500

    Create AdminPlatform page to host nested pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,6 @@ export * from "./admin-auth/sign-up/Signup";
 export * from "./admin-auth/sign-in/Signin";
 export * from "./admin-auth/forget-password/ForgetPassword";
 export * from "./admin-auth/reset-password/ResetPassword";
+
+export * from "./admin-platform/Platform";
+export * from "./admin-platform/projects/Projects";

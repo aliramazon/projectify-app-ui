61f05a26050958d769379b62f3256cec3f03ac96
Ali Ramazon <mr.safarali@gmail.com>
2024-01-24 09:38:07 -0500

    Team member can create a password

--- src/pages/team-member-auth/create-password/CreatePassword.tsx
+++ src/pages/team-member-auth/create-password/CreatePassword.tsx
@@ -5,6 +5,7 @@ import { useSearchParams, useNavigate } from "react-router-dom";
 import { Button, Input } from "../../../design-system";
 import { AuthActionLink, AuthWrapper } from "../../components";
 import brooklynBridge from "../../../assets/images/brooklyn-bridge.jpg";
+import { teamMember } from "../../../api";
 
 const Form = styled.form`
     width: 100%;
@@ -16,8 +17,8 @@ const Form = styled.form`
 const CreatePassword = () => {
     const [password, setPassword] = useState<string>("");
     const [passwordConfirm, setPasswordConfirm] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
     const [searchParams] = useSearchParams();
-    const passwordResetToken = searchParams.get("passwordResetToken");
 
     const navigate = useNavigate();
 
@@ -25,24 +26,41 @@ const CreatePassword = () => {
         setPassword(value);
     };
 
+    const handleOnChangeEmail = (value: string) => {
+        setEmail(value);
+    };
+
     const handleOnChangePasswordConfirm = (value: string) => {
         setPasswordConfirm(value);
     };
 
-    const resetPassword = async (e: React.FormEvent<HTMLFormElement>) => {
+    const createPassword = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        const inviteToken = searchParams.get("inviteToken");
+        console.log(inviteToken);
+        teamMember
+            .createPassword(
+                { email, password, passwordConfirm },
+                inviteToken as string
+            )
+            .then((data) => {
+                navigate("/team-member/sign-in");
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     };
 
     return (
         <AuthWrapper
             imageUrl={brooklynBridge}
-            pageTitle="Reset Password"
+            pageTitle="Create Password"
             switchLayout
         >
-            <Form onSubmit={resetPassword}>
+            <Form onSubmit={createPassword}>
                 <Input
                     type="password"
-                    placeholder="New Password"
+                    placeholder="Password"
                     value={password}
                     onChange={handleOnChangePassword}
                     shape="rounded"
@@ -50,15 +68,23 @@ const CreatePassword = () => {
                 />
                 <Input
                     type="password"
-                    placeholder="New Password Confirmation"
+                    placeholder="Password Confirmation"
                     value={passwordConfirm}
                     onChange={handleOnChangePasswordConfirm}
                     shape="rounded"
                     size="lg"
                 />
+                <Input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={handleOnChangeEmail}
+                    shape="rounded"
+                    size="lg"
+                />
 
                 <Button color="primary" size="lg" shape="rounded">
-                    Reset Password
+                    Create Password
                 </Button>
             </Form>
         </AuthWrapper>

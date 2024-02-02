ad8da9d93ab55f1b42e8b3f42235bc1fc6a7e343
Ali Ramazon <mr.safarali@gmail.com>
2024-02-01 23:44:01 -0700

    Add Action Menu to Task Card

--- src/pages/admin-platform/personal-tasks/PersonalTasks.tsx
+++ src/pages/admin-platform/personal-tasks/PersonalTasks.tsx
@@ -181,6 +181,10 @@ const Tasks = () => {
             });
     };
 
+    const onSelectTaskCardMenuAction = (value: string, taskId: string) => {
+        console.log(value, taskId);
+    };
+
     const groupedTasks = groupTasksByStatus(adminPersonalTasks);
 
     return (
@@ -239,6 +243,23 @@ const Tasks = () => {
                                             <TaskCard
                                                 key={task.id}
                                                 task={task}
+                                                menuActions={[
+                                                    {
+                                                        label: "Edit",
+                                                        value: "editTask",
+                                                        variant: "primary",
+                                                        iconName: "edit",
+                                                    },
+                                                    {
+                                                        label: "Delete",
+                                                        value: "deleteTask",
+                                                        variant: "danger",
+                                                        iconName: "delete",
+                                                    },
+                                                ]}
+                                                onSelectMenuAction={
+                                                    onSelectTaskCardMenuAction
+                                                }
                                             />
                                         );
                                     })}

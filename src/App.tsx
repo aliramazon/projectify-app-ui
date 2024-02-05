11bfd9fc39000dfb425fbbf44dcaa69cbed74087
Ali Ramazon <mr.safarali@gmail.com>
2024-02-04 22:18:59 -0700

    Create Checkbox component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 
 import { useState } from "react";
-import { OptionValue, Select } from "./design-system/Select";
+
+import { Checkbox } from "./design-system";
 
 const App = () => {
-    const [date, setDate] = useState<Date>();
-    const [value, setValue] = useState<OptionValue>("");
+    const [value, setValue] = useState(false);
     return (
         <div style={{ padding: "50px" }}>
             <h1>Welcome </h1>
@@ -15,20 +15,14 @@ const App = () => {
             <Link to="admin/forget-password">Forget Password</Link>
             <Link to="admin/reset-password">Reset Password</Link>
             <div>
-                <Select
-                    options={[
-                        { label: "Option1", value: "option1" },
-                        { label: "Option2", value: "option2" },
-                        { label: "Option3", value: "option3" },
-                        { label: "Option4", value: "option4" },
-                        { label: "Option5", value: "option5" },
-                        { label: "Option6", value: "option6" },
-                    ]}
-                    headerPlaceholder="Select Option"
-                    onSelect={(option) => setValue(option.value)}
-                    size="md"
-                    shape="circle"
-                    value={value}
+                <Checkbox
+                    label="Hello world"
+                    id="checkbox"
+                    checked={value}
+                    onChange={(value) => setValue(value)}
+                    shape="rounded"
+                    indeterminate
+                    position="end"
                 />
             </div>
         </div>

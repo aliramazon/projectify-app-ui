b360fcdee4f8a5afadfbb9d42e062208b1c38ee9
Ali Ramazon <mr.safarali@gmail.com>
2024-02-09 22:20:10 -0700

    Create Table Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Link } from "react-router-dom";
 
 import { useState } from "react";
 
-import { Checkbox, LinearProgress, Switch } from "./design-system";
+import { Badge, Checkbox, LinearProgress, Switch } from "./design-system";
+import { TableBodyCell } from "./design-system/Table";
 
 const App = () => {
     const [value, setValue] = useState(false);
@@ -10,53 +11,13 @@ const App = () => {
         <div style={{ padding: "50px" }}>
             <h1>Welcome </h1>
 
-            <Link to="team-member/sign-in">Sign in</Link>
+            <Link to="team-member/sign-in">Team member Sign in</Link>
             <Link to="admin/sign-in">Sign in</Link>
             <Link to="admin/forget-password">Forget Password</Link>
             <Link to="admin/reset-password">Reset Password</Link>
-            <div>
-                <Checkbox
-                    label="Hello world"
-                    id="checkbox"
-                    checked={value}
-                    onChange={(value) => setValue(value)}
-                    shape="rounded"
-                />
 
-                <Switch
-                    checked={value}
-                    onSwitch={(value) => setValue(value)}
-                    shape="circle"
-                    id="switch"
-                    position="end"
-                />
-            </div>
             <div>
-                <LinearProgress value={50} color="orange" error />
-                <LinearProgress value={40} color="blue" />
-                <LinearProgress value={40} color="green" />
-                <LinearProgress value={40} color="red" />
-                <LinearProgress
-                    value={50}
-                    color="orange"
-                    error
-                    shape="rounded"
-                />
-                <LinearProgress
-                    value={40}
-                    color="blue"
-                    shape="rounded"
-                    size="md"
-                />
-                <LinearProgress
-                    value={100}
-                    color="green"
-                    shape="rounded"
-                    size="lg"
-                />
-                <LinearProgress value={40} color="red" error shape="rounded" />
-
-                <LinearProgress value={99} />
+                <TableBodyCell align="center">Hello</TableBodyCell>
             </div>
         </div>
     );

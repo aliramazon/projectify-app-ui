5c78837b895f5efba4eae05e736645ccdf9eeaea
Ali Ramazon <mr.safarali@gmail.com>
2024-02-17 20:16:34 -0700

    Do not treat ISO 8601 as Date

--- src/pages/admin-platform/team-members/CreateTeamMemberModal.tsx
+++ src/pages/admin-platform/team-members/CreateTeamMemberModal.tsx
@@ -14,6 +14,7 @@ import {
 import { useStore } from "../../../hooks";
 import { Actions, AdminAddTeamMemberAction } from "../../../store";
 import { teamMemberService } from "../../../api";
+import { formatISO } from "date-fns";
 
 type ModalProps = {
     show: boolean;
@@ -91,7 +92,7 @@ const CreateTeamMemberModal: React.FC<ModalProps> = ({ show, closeModal }) => {
             firstName,
             lastName,
             email,
-            joinDate: joinDate!,
+            joinDate: formatISO(joinDate!),
             position: position?.value as string,
         };
         try {

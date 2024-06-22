d152a65837d73a46709825e9282a8d393c4cc51d
Ali Ramazon <mr.safarali@gmail.com>
2024-06-22 15:12:15 -0600

    TopNavigation: Add links to auth pages

--- src/pages/landing/sections/navigation/TopNavigation.tsx
+++ src/pages/landing/sections/navigation/TopNavigation.tsx
@@ -2,7 +2,10 @@ import styled from "styled-components";
 import { Container } from "../../components";
 import { Button, Logo } from "../../../../design-system";
 
-const NavigationLayout = styled.nav`
+const TopNavigationBase = styled.nav`
+    background-color: var(--primary-12);
+`;
+const NavigationLayout = styled.div`
     height: 7.2rem;
     display: flex;
     align-items: center;
@@ -21,58 +24,61 @@ const NavLinks = styled.div`
 
 export const TopNavigation = () => {
     return (
-        <Container>
-            <NavigationLayout>
-                <Logo layout="horizontal" size="sm" />
-                <NavLinks>
-                    <Button
-                        variant="text"
-                        size="sm"
-                        color="secondary"
-                        shape="rounded"
-                    >
-                        About
-                    </Button>
-                    <Button
-                        variant="text"
-                        size="sm"
-                        color="secondary"
-                        shape="rounded"
-                    >
-                        Testimonials
-                    </Button>
-                    <Button
-                        variant="text"
-                        size="sm"
-                        color="secondary"
-                        shape="rounded"
-                    >
-                        Contact
-                    </Button>
-                </NavLinks>
-                <AuthLinks>
-                    <Button
-                        variant="outlined"
-                        color="primary"
-                        size="sm"
-                        shape="rounded"
-                        renderAs="link"
-                        navigateTo="admin/sign-up"
-                    >
-                        Sign up
-                    </Button>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        size="sm"
-                        shape="rounded"
-                        renderAs="link"
-                        navigateTo="admin/sign-in"
-                    >
-                        Login
-                    </Button>
-                </AuthLinks>
-            </NavigationLayout>
-        </Container>
+        <TopNavigationBase>
+            {" "}
+            <Container>
+                <NavigationLayout>
+                    <Logo layout="horizontal" size="sm" />
+                    <NavLinks>
+                        <Button
+                            variant="text"
+                            size="sm"
+                            color="secondary"
+                            shape="rounded"
+                        >
+                            About
+                        </Button>
+                        <Button
+                            variant="text"
+                            size="sm"
+                            color="secondary"
+                            shape="rounded"
+                        >
+                            Testimonials
+                        </Button>
+                        <Button
+                            variant="text"
+                            size="sm"
+                            color="secondary"
+                            shape="rounded"
+                        >
+                            Contact
+                        </Button>
+                    </NavLinks>
+                    <AuthLinks>
+                        <Button
+                            variant="outlined"
+                            color="primary"
+                            size="sm"
+                            shape="rounded"
+                            renderAs="link"
+                            navigateTo="admin/sign-up"
+                        >
+                            Sign up
+                        </Button>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            size="sm"
+                            shape="rounded"
+                            renderAs="link"
+                            navigateTo="admin/sign-in"
+                        >
+                            Login
+                        </Button>
+                    </AuthLinks>
+                </NavigationLayout>
+            </Container>
+        </TopNavigationBase>
     );
 };
